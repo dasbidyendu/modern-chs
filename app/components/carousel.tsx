0d886fcb0d5c0c6b8e353644e74cfe7f525b9e08
dasbidyendu <150836749+Bidyendu221@users.noreply.github.com>
2024-08-31 11:16:33 +0530

    carousel updated

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import { useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   return (
-    <div className="carousel w-full h-[700px] mb-[50px] flex flex-nowrap overflow-x-scroll overscroll-none">
-      <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
-      <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
-      <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
-      <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
+    <div className="carousel w-full h-[700px] mb-[50px] relative ">
+      <button className="">
+        <FaArrowRight />
+      </button>
+      <div className="items-holder flex flex-nowrap overflow-x-hidden overflow-y-hidden overscroll-none">
+        <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
+        <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
+        <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
+        <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
+      </div>
+      <button>
+        <FaArrowLeft />
+      </button>
     </div>
   );
 };

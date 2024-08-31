60dfec0362e551910345ffdcfeb880ca298ba19e
dasbidyendu <150836749+Bidyendu221@users.noreply.github.com>
2024-08-31 11:34:47 +0530

    added carousel slider arrows

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -5,10 +5,17 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClickRight = () => {};
+  const handleClickLeft = () => {};
+
   return (
     <div className="carousel w-full h-[700px] mb-[50px] relative ">
-      <button className="">
-        <FaArrowRight />
+      <button
+        className="absolute top-0 right-0 translate-x-[-20px] translate-y-[350px]"
+        onClick={handleClickRight}
+      >
+        <FaArrowRight color="white" size={30} />
       </button>
       <div className="items-holder flex flex-nowrap overflow-x-hidden overflow-y-hidden overscroll-none">
         <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
@@ -16,8 +23,11 @@ const Carousel = () => {
         <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
         <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
       </div>
-      <button>
-        <FaArrowLeft />
+      <button
+        className="absolute top-0 left-0 translate-x-[20px] translate-y-[350px]"
+        onClick={handleClickLeft}
+      >
+        <FaArrowLeft color="white" size={30} />
       </button>
     </div>
   );

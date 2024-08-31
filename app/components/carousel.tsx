8cc9048985f07542cb4a677617ff116fe9aa14d7
dasbidyendu <150836749+Bidyendu221@users.noreply.github.com>
2024-08-31 12:15:09 +0530

    used swiper for carousel

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,35 +1,31 @@
 "use client";
 
 import { useState } from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const Carousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handleClickRight = () => {};
-  const handleClickLeft = () => {};
-
   return (
-    <div className="carousel w-full h-[700px] mb-[50px] relative ">
-      <button
-        className="absolute top-0 right-0 translate-x-[-20px] translate-y-[350px]"
-        onClick={handleClickRight}
-      >
-        <FaArrowRight color="white" size={30} />
-      </button>
-      <div className="items-holder flex flex-nowrap overflow-x-hidden overflow-y-hidden overscroll-none">
-        <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
-        <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
-        <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
-        <div className="item item-a w-dvw h-[700px] flex justify-center items-center"></div>
-      </div>
-      <button
-        className="absolute top-0 left-0 translate-x-[20px] translate-y-[350px]"
-        onClick={handleClickLeft}
-      >
-        <FaArrowLeft color="white" size={30} />
-      </button>
-    </div>
+    <Swiper
+      spaceBetween={5}
+      slidesPerView={1}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+      className="mb-[50px]"
+    >
+      <SwiperSlide>
+        <div className="w-dvw h-[800px] overflow-hidden  flex justify-center items-center item-a "></div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="w-dvw h-[800px] overflow-hidden  flex justify-center items-center item-a "></div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="w-dvw h-[800px]  overflow-hidden flex justify-center items-center item-a "></div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="w-dvw h-[800px] overflow-hidden   flex justify-center items-center item-a "></div>
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
